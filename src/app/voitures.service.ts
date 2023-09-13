7bee25cdfcd74add2e4c8b9ccf61cd94379c35d7
MedAmine <med.amine.mzk@horizon-tech.tn>
2023-09-13 09:19:10 +0100

    update service

--- src/app/voitures.service.ts
+++ src/app/voitures.service.ts
@@ -9,21 +9,12 @@ import { HttpClient} from "@angular/common/http";
 export class VoituresService {
   private baseURL="http://localhost:8000/api/voitures";
 
-  //voitureChanged = new Subject<Voiture[]>();
-
   constructor(private http: HttpClient) { }
 
   /**Get voitures */
   getVoitures():Observable<any>{
     return this.http.get<Voiture[]>(this.baseURL);
   }
-
-  getVoituresUPDATED():Observable<any>{
-    return  this.http.get(this.baseURL);
-  }
-
-
-
 
   /**Get voiture */
   getVoiture(id:number):Observable<any>{
@@ -34,23 +25,15 @@ export class VoituresService {
   /**Post voiture */
   postVoiture(voiture: Voiture): Observable<Voiture>{
     return this.http.post<Voiture>(this.baseURL, voiture)
-    //.pipe(tap(v => this.notifyVoitureChanged())    )
  }
 
   /**PUT: mettre à jour la voiture dans le serveur */
   updateVoiture(voiture: Voiture): Observable<Voiture>{
      return this.http.put<Voiture>(this.baseURL+'/'+voiture.id, voiture)
-     //.pipe( tap(v => this.notifyVoitureChanged()));
   }
 
   deleteVoiture(voiture: Voiture):Observable<String>{
     return this.http.delete<String>(this.baseURL+'/'+voiture.id)
-    //.pipe( tap(v => this.notifyVoitureChanged()));
   }
 
-  // notifyVoitureChanged(){
-  //   this.getVoitures().subscribe(
-  //     voitures => this.voitureChanged.next(voitures)
-  //   )
-  // }
 }
